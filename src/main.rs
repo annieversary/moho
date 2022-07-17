38763a543d42cfc8c03ceb07e5e7614143cb7468
annieversary <annie@versary.town>
2022-07-17 12:07:20 +0100

    mvp

---
+++ src/main.rs
@@ -0,0 +1,388 @@
+use clap::Parser;
+use color_eyre::eyre::Result;
+use std::{
+    fs::{self, File},
+    io::Write,
+    os::unix::prelude::PermissionsExt,
+    path::PathBuf,
+};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(subcommand)]
+    action: Action,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Action {
+    Create {
+        #[clap(value_parser)]
+        name: String,
+        #[clap(value_parser)]
+        default_path: Option<PathBuf>,
+    },
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let args = Args::parse();
+
+    match args.action {
+        Action::Create { name, default_path } => create_template(name, default_path),
+    }
+}
+
+fn create_template(name: String, default_path: Option<PathBuf>) -> Result<()> {
+    // TODO check if we're getting text from stdin
+    let template = edit::edit("write your template here...")?;
+
+    let parsed = parse_template(&template);
+
+    // TODO ask about defaults/descriptions
+
+    // generate bash script
+    let out = generate_bash_script(parsed, default_path);
+
+    // save to file
+    let out_path = format!(".moho/{name}.mh");
+    std::fs::create_dir_all(".moho")?;
+    let mut file = File::create(&out_path)?;
+    file.write_all(out.as_bytes())?;
+    make_executable(&out_path)?;
+
+    Ok(())
+}
+
+fn make_executable(path: &str) -> Result<()> {
+    let mut perms = fs::metadata(path)?.permissions();
+    perms.set_mode(0o755);
+    fs::set_permissions(path, perms)?;
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct Template<'a> {
+    original: &'a str,
+    generated: String,
+    // variable -> default value
+    variables: Vec<Variable<'a>>,
+    filtered: Vec<FilteredVariable<'a>>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Variable<'a> {
+    variable: &'a str,
+    default: Option<String>,
+    description: Option<String>,
+}
+
+impl<'a> Variable<'a> {
+    pub fn new(variable: &'a str) -> Self {
+        Self {
+            variable,
+            default: None,
+            description: None,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FilteredVariable<'a> {
+    variable: &'a str,
+    filters: Vec<&'a str>,
+    /// name for the filtered variable
+    name: String,
+}
+
+fn parse_template<'a>(template: &'a str) -> Template<'a> {
+    let mut generated = String::with_capacity(template.len());
+    let mut variables: Vec<Variable<'a>> = Vec::new();
+    let mut filtered: Vec<FilteredVariable> = Vec::new();
+
+    let chars = template.chars();
+    let mut last_char = None;
+
+    let mut var: Option<usize> = None;
+
+    for (i, c) in chars.enumerate() {
+        // starting variable
+        if var.is_none() && last_char == Some('{') && c == '{' {
+            last_char = None;
+            var = Some(i + 1);
+            continue;
+        }
+
+        // ending variable
+        if last_char == Some('}') && c == '}' {
+            if let Some(start) = var.take() {
+                let var_str = &template[start..i - 1].trim();
+
+                generated.push_str("${");
+                if var_str.contains('|') {
+                    let f = parse_filtered_variable(&var_str);
+                    generated.push_str(&f.name);
+
+                    variables.push(Variable::new(f.variable));
+                    filtered.push(f);
+                } else {
+                    generated.push_str(var_str);
+                    variables.push(Variable::new(var_str));
+                }
+                generated.push('}');
+
+                last_char = None;
+                continue;
+            }
+        }
+
+        if var.is_none() {
+            if let Some(l) = last_char {
+                generated.push(l);
+            }
+        }
+
+        last_char = Some(c);
+    }
+
+    Template {
+        original: template,
+        generated,
+        variables,
+        filtered,
+    }
+}
+
+fn parse_filtered_variable<'a>(v: &'a str) -> FilteredVariable<'a> {
+    let mut vals = v.split('|').map(|s| s.trim());
+
+    let name = vals.clone().collect::<Vec<_>>().join("_");
+    let variable = vals.next().expect("variable to have at least one value");
+    let filters = vals.collect();
+
+    FilteredVariable {
+        variable,
+        filters,
+        name,
+    }
+}
+
+fn generate_bash_script(t: Template, default_path: Option<PathBuf>) -> String {
+    let mut script = String::from("#!/bin/sh\nset -e\n\n");
+
+    macro_rules! append {
+        ( $($s:expr),* $(,)? ) => {
+            $(script.push_str($s);)*
+        };
+    }
+
+    // generate variables
+    append!("# variable declarations\n");
+    for v in &t.variables {
+        append!(v.variable, "=''\n");
+    }
+
+    // generate argument parsing
+    append!(
+        r#"
+# parse arguments
+while test $# -gt 0; do
+  case "$1" in
+"#,
+    );
+    for v in &t.variables {
+        append!(
+            "    --",
+            v.variable,
+            ")\n      shift\n      ",
+            v.variable,
+            r#"="$1"
+      shift
+      ;;
+"#,
+        );
+    }
+
+    append!(
+        r#"    *)
+      echo "options:"
+      echo "-h, --help                show brief help"
+"#,
+    );
+
+    for v in &t.variables {
+        append!(
+            r#"      echo "--"#,
+            v.variable,
+            " ",
+            &v.variable.to_uppercase(),
+            "\"\n"
+        );
+    }
+
+    append!(
+        r#"      exit 0
+      ;;
+  esac
+done
+"#,
+    );
+
+    // defaults if there are any
+    if t.variables.iter().any(|v| v.default.is_some()) {
+        append!("\n# set variable defaults\n");
+    }
+    for v in &t.variables {
+        if let Some(default) = &v.default {
+            append!(v.variable, "=${", v.variable, ":-\"", default, "\"}\n");
+        }
+    }
+
+    // check that all variables have values
+    append!("\n# check that all variables have values\n");
+    for v in &t.variables {
+        append!(
+            "if [[ -z \"$",
+            v.variable,
+            "\" ]]; then\n  echo \"No value provided for ",
+            v.variable,
+            "\"\n  exit 1\nfi\n"
+        );
+    }
+
+    if !t.filtered.is_empty() {
+        // get all the used filters
+        let filters = get_filters(
+            &t.filtered
+                .iter()
+                .flat_map(|a| a.filters.clone())
+                .collect::<Vec<_>>(),
+        );
+        append!("\n# filters\n", &filters);
+
+        append!("\n# filtered variables\n");
+        for v in &t.filtered {
+            append!(&v.name, "=");
+            for filter in &v.filters {
+                append!("$(", filter, " ");
+            }
+            append!("\"$", v.variable, "\"");
+            for _ in &v.filters {
+                append!(")");
+            }
+            append!("\n");
+        }
+    }
+    append!("\necho \"", &t.generated, "\"\n");
+
+    script
+}
+
+fn get_filters(_filters: &[&str]) -> String {
+    // TODO properly implement this
+    // not sure if we wanna have like a stdlib or smth
+
+    r#"upper() {
+  echo $(echo "$1" | tr '[:lower:]' '[:upper:]')
+}
+"#
+    .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn parse() {
+        let out = parse_template("hello {{ hi }} {{ hey | upper }} hii");
+
+        assert_eq!(out.generated, "hello ${hi} ${hey_upper} hi");
+
+        // check variables
+        assert_eq!(
+            out.variables
+                .into_iter()
+                .map(|v| v.variable)
+                .collect::<Vec<_>>(),
+            vec!["hi", "hey"]
+        );
+
+        // check filtered
+        assert_eq!(
+            out.filtered,
+            vec![FilteredVariable {
+                variable: "hey",
+                filters: vec!["upper"],
+                name: "hey_upper".to_string()
+            }]
+        );
+    }
+
+    #[test]
+    fn parse_and_generate() {
+        let mut out = parse_template("hello {{ hi }} {{ hey | upper }} hii");
+        out.variables.first_mut().unwrap().default = Some("meooow".to_string());
+        let out = generate_bash_script(out);
+
+        assert_eq!(
+            out,
+            r#"#!/bin/sh
+set -e
+
+# variable declarations
+hi=''
+hey=''
+
+# parse arguments
+while test $# -gt 0; do
+  case "$1" in
+    --hi)
+      shift
+      hi="$1"
+      shift
+      ;;
+    --hey)
+      shift
+      hey="$1"
+      shift
+      ;;
+    *)
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "--hi HI"
+      echo "--hey HEY"
+      exit 0
+      ;;
+  esac
+done
+
+# set variable defaults
+hi=${hi:-"meooow"}
+
+# check that all variables have values
+if [[ -z "$hi" ]]; then
+  echo "No value provided for hi"
+  exit 1
+fi
+if [[ -z "$hey" ]]; then
+  echo "No value provided for hey"
+  exit 1
+fi
+
+# filters
+upper() {
+  echo $(echo "$1" | tr '[:lower:]' '[:upper:]')
+}
+
+# filtered variables
+hey_upper=$(upper "$hey")
+
+echo "hello ${hi} ${hey_upper} hi"
+"#
+        )
+    }
+}
